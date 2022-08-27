daee81155db2cee503ece68b3c6e8ae901fc103a
julia <juliabresolin2@gmail.com>
2022-08-27 13:39:48 -0300

    style: correct location container height

--- src/components/Location/styles.ts
+++ src/components/Location/styles.ts
@@ -16,7 +16,7 @@ export const LocationContainer = styled.div`
   div {
     width: 100%;
     max-width: 32rem;
-    max-height: 33rem;
+    max-height: 35rem;
     border-radius: 6px;
     padding: 2rem;
 
