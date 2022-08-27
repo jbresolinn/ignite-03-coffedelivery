4cc71526a2c06179ed321b9d31b245e3daf7f48a
julia <juliabresolin2@gmail.com>
2022-08-26 22:40:15 -0300

    feat: create global style && add routes

---
+++ src/styles/global.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body {
+    background: ${(props) => props.theme['gray-100']};
+    color: ${(props) => props.theme['gray-800']};
+    -webkit-font-smoothing: antialiased;
+}
+
+body, input, textarea, button {
+    font: 400 1rem 'Roboto', sans-serif;
+}
+
+a, button {
+    cursor: pointer;
+}
+
+button:disabled {
+    cursor: not-allowed;
+}
+`
